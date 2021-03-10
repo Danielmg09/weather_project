8238b424694e071a8f80aee168f3f488a4cef547
Daniel <Danielmartinezgarciadmg@gmail.com>
2021-03-10 23:50:01 +0100

    threads modified

--- src/main/java/com/tecnara/weather/client/ClientThread.java
+++ src/main/java/com/tecnara/weather/client/ClientThread.java
@@ -7,7 +7,12 @@ public class ClientThread extends Thread{
     public void run() {
         Client client = new Client();
 
+        //coordinates example
         String coordinates = "{\"lon\":13.5, \"lat\":13.3}";
+
+        //Uncomment to ask coordinates
+        //String coordinates = client.askCoordinates();
+
 
         if (Utils.checker(coordinates)){
             client.sendRequest(coordinates);
--- src/main/java/com/tecnara/weather/client/Main.java
+++ src/main/java/com/tecnara/weather/client/Main.java
@@ -4,6 +4,28 @@ import com.tecnara.weather.client.utils.Utils;
 
 public class Main {
     public static void main(String[] args) {
+        //Without threads
+        /*
+        Client client = new Client();
+
+        //coordinates example
+        String coordinates = "{\"lon\":13.5, \"lat\":13.3}";
+
+        //Uncomment to ask coordinates
+        //String coordinates = client.askCoordinates();
+
+
+        if (Utils.checker(coordinates)){
+            client.sendRequest(coordinates);
+            System.out.println(client.getResponse());
+        }else{
+            System.out.println("This coordinates are not correct");
+        }
+
+        client.close();
+        */
+
+
 
         ClientThread c1 = new ClientThread();
         ClientThread c2 = new ClientThread();
